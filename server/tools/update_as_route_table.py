75c7f59f2139116d5481c406a025862c190905a8
Potat0 <i@potat0.cc>
2024-03-08 11:56:13 +0800

    从 ROA 中去重

--- server/tools/update_as_route_table.py
+++ server/tools/update_as_route_table.py
@@ -7,9 +7,9 @@ import requests
 def update_as_route_table():
     try:
         roas = requests.get('https://dn42.burble.com/roa/dn42_roa_46.json', timeout=5).json()
-        AS_ROUTE = defaultdict(list)
+        AS_ROUTE = defaultdict(set)
         for roa in roas['roas']:
-            AS_ROUTE[int(roa['asn'][2:])].append(roa['prefix'])
+            AS_ROUTE[int(roa['asn'][2:])].add(roa['prefix'])
         base.AS_ROUTE = dict(AS_ROUTE)
     except BaseException:
         pass
