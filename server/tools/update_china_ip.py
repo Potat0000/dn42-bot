a39e76443fa57ab80922a69af1f75862a8beba98
Potat0 <i@potat0.cc>
2025-10-07 14:08:58 +0800

    更换 China IP 数据源

--- server/tools/update_china_ip.py
+++ server/tools/update_china_ip.py
@@ -6,16 +6,16 @@ import requests
 
 def update_china_ip():
     try:
-        ChinaIPv4_RAW = requests.get(
-            "https://raw.githubusercontent.com/misakaio/chnroutes2/master/chnroutes.txt", timeout=30
-        ).text
+        ChinaIPv4_RAW = requests.get("https://ruleset.skk.moe/Clash/ip/china_ip.txt", timeout=30).text
         base.ChinaIPv4 = [
             IPv4Network(i.strip()) for i in ChinaIPv4_RAW.splitlines() if i != "" and not i.startswith("#")
         ]
     except BaseException:
         pass
     try:
-        ChinaIPv6_RAW = requests.get("https://ispip.clang.cn/all_cn_ipv6.txt", timeout=30).text
-        base.ChinaIPv6 = [IPv6Network(i.strip()) for i in ChinaIPv6_RAW.splitlines() if i != ""]
+        ChinaIPv6_RAW = requests.get("https://ruleset.skk.moe/Clash/ip/china_ip_ipv6.txt", timeout=30).text
+        base.ChinaIPv6 = [
+            IPv6Network(i.strip()) for i in ChinaIPv6_RAW.splitlines() if i != "" and not i.startswith("#")
+        ]
     except BaseException:
         pass
